6a902276554ff4f20561acd56b49470c34205f77
Munaiz <50893239+Munaiz123@users.noreply.github.com>
2019-07-31 15:33:51 -0500

    Update double.js

--- src/pages/Components/double.js
+++ src/pages/Components/double.js
@@ -1,3 +1,9 @@
-function doubleString (string){
-    return string + string;
+function doubleChar (string){
+    let newString = '';
+    for(let i  = 0; i < string.length; i++){
+        let char = string[i];
+        if(char !== ' ') newString += char+char;
+        if(char === ' ') newString += char;
+    }
+    return newString;
 }
