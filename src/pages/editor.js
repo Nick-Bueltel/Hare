1a82957b00023952835124e6ed7578a5f40a0cb0
Nick Bueltel <nickbueltel@gmail.com>
2019-07-31 13:06:02 -0500

    add dialog from electron

--- src/pages/editor.js
+++ src/pages/editor.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Editor, EditorState, RichUtils } from 'draft-js';
-import createMarkdownPlugin from 'draft-js-markdown-plugin';
+import { Editor, EditorState, RichUtils } from 'draft-js'
+import createMarkdownPlugin from 'draft-js-markdown-plugin'
+import dialog from 'electron'
+
 
 class MyEditor extends React.Component {
     state = {
