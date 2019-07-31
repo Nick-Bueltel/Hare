1f8cedace4bcad808438ca8d670e650fc8997652
Nick Bueltel <nickbueltel@gmail.com>
2019-07-31 13:13:03 -0500

    fix error in app.js

--- src/pages/editor.js
+++ src/pages/editor.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Editor, EditorState, RichUtils } from 'draft-js'
 import createMarkdownPlugin from 'draft-js-markdown-plugin'
-import dialog from 'electron'
 
 
 class MyEditor extends React.Component {
